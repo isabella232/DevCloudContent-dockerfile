fb83d56519098e15910e0e972b2ecc8ed629178c
Nihal Winston D'Cunha <nihal.winston.dcunha@intel.com>
2021-09-30 14:26:47 +0530

    updated license

--- openvino-tensorflow/run.sh
+++ openvino-tensorflow/run.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
+# ==============================================================================
+# Copyright (C) 2021 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+# ==============================================================================
 echo "Running inference with OpenVINO-TensorFlow"
 echo "Running inference with OpenVINO-TensorFlow" > /app/result/with_ov-tf.txt
 python3 examples/classification_sample.py | tee -a /app/result/with_ov-tf.txt
