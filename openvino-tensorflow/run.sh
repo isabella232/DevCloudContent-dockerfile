6bb6fdb85030db6919dada8c1adf7f09094e51d1
Nihal Winston D'Cunha <nihal.winston.dcunha@intel.com>
2021-11-12 20:19:22 +0530

    Update run.sh
    updated the way of writing output to the file and displaying

--- openvino-tensorflow/run.sh
+++ openvino-tensorflow/run.sh
@@ -5,7 +5,9 @@
 # ==============================================================================
 echo "Running inference with OpenVINO-TensorFlow"
 echo "Running inference with OpenVINO-TensorFlow" > /app/result/with_ov-tf.txt
-python3 examples/classification_sample.py 2>&1 | tee -a /app/result/with_ov-tf.txt
+python3 examples/classification_sample.py >> /app/result/with_ov-tf.txt
+cat /app/result/with_ov-tf.txt
 echo "Running inference with TensorFlow"
 echo "Running inference with TensorFlow" > /app/result/without_ov-tf.txt
-python3 examples/classification_sample.py --disable_ovtf 2>&1 | tee -a /app/result/without_ov-tf.txt
+python3 examples/classification_sample.py --disable_ovtf >> /app/result/without_ov-tf.txt
+cat /app/result/without_ov-tf.txt
