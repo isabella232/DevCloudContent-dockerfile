29fd8d8764174d7751eb8c1cc187b2943a2b1c28
Nihal Winston D'Cunha <nihal.winston.dcunha@intel.com>
2021-11-12 21:53:35 +0530

    Update run.sh

--- openvino-tensorflow/run.sh
+++ openvino-tensorflow/run.sh
@@ -5,9 +5,7 @@
 # ==============================================================================
 echo "Running inference with OpenVINO-TensorFlow"
 echo "Running inference with OpenVINO-TensorFlow" > /app/result/with_ov-tf.txt
-python3 examples/classification_sample.py >> /app/result/with_ov-tf.txt
-cat /app/result/with_ov-tf.txt
+python3 examples/classification_sample.py
 echo "Running inference with TensorFlow"
 echo "Running inference with TensorFlow" > /app/result/without_ov-tf.txt
-python3 examples/classification_sample.py --disable_ovtf >> /app/result/without_ov-tf.txt
-cat /app/result/without_ov-tf.txt
+python3 examples/classification_sample.py --disable_ovtf
